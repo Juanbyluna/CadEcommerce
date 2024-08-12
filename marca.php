39db8e997a321a78418c523f564641d067596360
juanbyluna <juanfernandesdegoes@gmail.com>
2024-08-12 11:26:36 -0300

    adiciona arquivos que não estavam presentes no projeto

---
+++ marca.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, inicial-scale=1.0">
+    <title>Cadastrar Marca</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+        <div class="center">
+            <h1>Cadastro de Marca</h1>
+            <a href="index.php" target="_self"> Voltar </a>
+        </div>
+</header>
+        <section id="produtos">
+            <form action="insere-marca.php" method="post">
+                <label for="">Descrição</label><br>
+                <input type="text" name="descricao"><br><br>
+                <input type="submit" value="Cadatrar"><br>
+
+            </form>
+        </section>
+
+  
+         
+</body>
+</html>
